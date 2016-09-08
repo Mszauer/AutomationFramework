012a8204703b52e89a5f2059f2ff2073a6bb95cb
mszauer <mszauer@mail.usf.edu>
2016-09-08 08:27:52 -0700

    added button to listbox to view source code

--- AutomationFramework/Program.cs
+++ AutomationFramework/Program.cs
@@ -1,4 +1,5 @@
 ﻿//#define debugColors
+//#define debugSplitter
 using System;
 using System.Windows.Forms;
 using System.IO;
@@ -13,6 +14,7 @@ namespace Game {
         public WebBrowser browser;
         public ListBox currentPages;
         public TabControl tabs;
+        
 
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.ComponentModel.IContainer components = null;
@@ -58,14 +60,14 @@ namespace Game {
 
             currentPages = new ListBox();
             CreateListBox(bodySplitter);
-            //RefreshListBox();stuck in a loop
+            //RefreshListBox(); //works but doesn't display items
 
             tabs = new TabControl();
             tabs.Dock = DockStyle.Fill;
             bodySplitter.Panel2.Controls.Add(tabs);
 
             AddTab(this, new EventArgs());
-
+            tabs.SelectedTab.Controls.Add(browser);
 
             #region DebugColors
 #if debugColors
@@ -79,29 +81,48 @@ namespace Game {
 
         }
         void CreateListBox(SplitContainer rootSplitter) {
+            SplitContainer splitListBox = ListboxSeperate();
+
+
+            rootSplitter.Panel1.Controls.Add(splitListBox);
+            #region Listbox
             ListBox list = new ListBox();
-            rootSplitter.Panel1.Controls.Add(list);
-
             list.Dock = DockStyle.Fill;
-            list.SelectionMode = SelectionMode.One;
-
+            list.SelectionMode = SelectionMode.One; splitListBox.Panel1.Controls.Add(list);
+            #endregion
+            #region Sourcecode View Button
+            Button sourceCodeViewButton = new Button();
+            sourceCodeViewButton.Text = "View Source Code";
+            sourceCodeViewButton.Dock = DockStyle.Fill;
+            sourceCodeViewButton.BackColor = Color.LightGreen;
+            splitListBox.Panel2.Controls.Add(sourceCodeViewButton);
+            #endregion
+            #region SplitterDistanceDebug
+#if debugSplitter
+            splitListBox.SplitterMoved += delegate {
+                sourceCodeViewButton.Text = ("Splitter Distance: " + splitListBox.SplitterDistance.ToString());
+            };
+#endif
+            #endregion
         }
         protected void RefreshListBox() {
-            foreach(HtmlWindow window in RefreshFrames()) {
+            /*foreach(HtmlWindow window in RefreshFrames()) {
                 currentPages.Items.Add(window);
+            }*/
+            foreach(String test in RefreshFrames()) {
+                currentPages.Items.Add(test);
+                //currentPages.Controls.Add(test);
             }
-
         }
         protected void SwitchTabs(object sender, EventArgs e) {
-            //insert switch tab logic here
+            //switches depending on selected index of currentPages
+            //tabs.SelectedTab = currentPages.SelectedIndex ; //change to match with index
         }
         protected void AddTab(object sender, EventArgs e) {
             TabPage defaultTab = new TabPage();
             defaultTab.Name = browser.DocumentTitle;
             tabs.TabPages.Add(defaultTab);
             tabs.SelectTab(tabs.TabPages.Count - 1);
-
-            defaultTab.Controls.Add(browser);
         }
         protected void RemoveTab(object sender, EventArgs e) {
             if (!(tabs.TabPages.Count == 1)){
@@ -131,14 +152,14 @@ namespace Game {
             string textboxURL;
             rootSplitter.SplitterDistance = (int)(3.5f * (rootSplitter.Size.Width / 4));
 
-            #region URLFIELD
+#region URLFIELD
             TextBox urlText = new TextBox();
             urlText.Dock = DockStyle.Fill;
             urlText.Multiline = false;
             rootSplitter.Panel1.Controls.Add(urlText);
-            #endregion
+#endregion
 
-            #region GO/DOWNLOAD BUTTONS
+#region GO/DOWNLOAD BUTTONS
             SplitContainer goDownloadSplit = new SplitContainer();
             goDownloadSplit.SplitterDistance = rootSplitter.Width / 2;
             goDownloadSplit.IsSplitterFixed = true;
@@ -170,7 +191,7 @@ namespace Game {
                 GetHTMLBody();
             };
             goDownloadSplit.Panel2.Controls.Add(download);
-            #endregion
+#endregion
         }
         void OnWebpageLoad(object sender, WebBrowserDocumentCompletedEventArgs e) {
             browser.Document.GetElementById("lst-ib").SetAttribute("value", "Superman");//insert text for search boxes
@@ -208,10 +229,29 @@ namespace Game {
 
             }
         }
-        List<HtmlWindow> RefreshFrames() {
+        SplitContainer ListboxSeperate() {
+            SplitContainer listboxSplitter = new SplitContainer();
+            listboxSplitter.Dock = DockStyle.Fill;
+            listboxSplitter.IsSplitterFixed = true;
+            listboxSplitter.Panel2MinSize = 5;
+            listboxSplitter.SplitterDistance =  725;
+            listboxSplitter.Orientation = Orientation.Horizontal;
+#region DebugColors
+#if debugColors
+            listboxSplitter.Panel1.BackColor = Color.Coral;
+            listboxSplitter.Panel2.BackColor = Color.Chocolate;
+#endif
+#endregion
+            return listboxSplitter;
+        }
+        /*List<HtmlWindow> RefreshFrames() {
             List<HtmlWindow> currentWindows = GetIFrames(browser.Document.Window);
 
             return currentWindows;
+        } //this gets stuck on infinte loop*/
+        String[] RefreshFrames() {
+            string[] tester = new string[3] { "one", "two", "3" };
+            return tester;
         }
         protected List<HtmlWindow> GetIFrames(HtmlWindow rootWindow) {
             List<HtmlWindow> result = new List<HtmlWindow>();
